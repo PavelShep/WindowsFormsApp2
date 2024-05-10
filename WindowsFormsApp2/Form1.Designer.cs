25dd8d0184560699bddf858341f394229160d888
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2024-05-10 12:46:11 +0200

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewVillas = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDetails = new System.Windows.Forms.TextBox();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.txtSqft = new System.Windows.Forms.TextBox();
+            this.txtOccupancy = new System.Windows.Forms.TextBox();
+            this.txtImageUrl = new System.Windows.Forms.TextBox();
+            this.txtAmenity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listViewVillas
+            // 
+            this.listViewVillas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listViewVillas.HideSelection = false;
+            this.listViewVillas.Location = new System.Drawing.Point(12, 12);
+            this.listViewVillas.Name = "listViewVillas";
+            this.listViewVillas.Size = new System.Drawing.Size(776, 234);
+            this.listViewVillas.TabIndex = 0;
+            this.listViewVillas.UseCompatibleStateImageBehavior = false;
+            this.listViewVillas.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Id";
+            this.columnHeader1.Width = 40;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Details";
+            this.columnHeader3.Width = 200;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Rate";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Sqft";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Occupancy";
+            this.columnHeader6.Width = 100;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "ImageUrl";
+            this.columnHeader7.Width = 100;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Amenity";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(184, 252);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(329, 252);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(475, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(240, 314);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtDetails
+            // 
+            this.txtDetails.Location = new System.Drawing.Point(240, 340);
+            this.txtDetails.Name = "txtDetails";
+            this.txtDetails.Size = new System.Drawing.Size(100, 20);
+            this.txtDetails.TabIndex = 5;
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(240, 366);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(100, 20);
+            this.txtRate.TabIndex = 6;
+            // 
+            // txtSqft
+            // 
+            this.txtSqft.Location = new System.Drawing.Point(240, 392);
+            this.txtSqft.Name = "txtSqft";
+            this.txtSqft.Size = new System.Drawing.Size(100, 20);
+            this.txtSqft.TabIndex = 7;
+            // 
+            // txtOccupancy
+            // 
+            this.txtOccupancy.Location = new System.Drawing.Point(432, 329);
+            this.txtOccupancy.Name = "txtOccupancy";
+            this.txtOccupancy.Size = new System.Drawing.Size(100, 20);
+            this.txtOccupancy.TabIndex = 8;
+            // 
+            // txtImageUrl
+            // 
+            this.txtImageUrl.Location = new System.Drawing.Point(432, 355);
+            this.txtImageUrl.Name = "txtImageUrl";
+            this.txtImageUrl.Size = new System.Drawing.Size(100, 20);
+            this.txtImageUrl.TabIndex = 9;
+            // 
+            // txtAmenity
+            // 
+            this.txtAmenity.Location = new System.Drawing.Point(432, 381);
+            this.txtAmenity.Name = "txtAmenity";
+            this.txtAmenity.Size = new System.Drawing.Size(100, 20);
+            this.txtAmenity.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Details";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(199, 370);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Rate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(199, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Sqft";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(364, 332);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Occupancy";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(377, 358);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "ImageUrl";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(391, 384);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Amenity";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAmenity);
+            this.Controls.Add(this.txtImageUrl);
+            this.Controls.Add(this.txtOccupancy);
+            this.Controls.Add(this.txtSqft);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.txtDetails);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listViewVillas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewVillas;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDetails;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.TextBox txtSqft;
+        private System.Windows.Forms.TextBox txtOccupancy;
+        private System.Windows.Forms.TextBox txtImageUrl;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.TextBox txtAmenity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private const string apiUrl = "https://localhost:7239/api/VillaAPI";
+        private HttpClient httpClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            httpClient = new HttpClient();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await LoadVillas();
+        }
+
+        private async Task LoadVillas()
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    List<Villa> villas = JsonConvert.DeserializeObject<List<Villa>>(json);
+                    DisplayVillas(villas);
+                }
+                else
+                {
+                    MessageBox.Show("Failed to load villas: " + response.ReasonPhrase, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load villas: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DisplayVillas(List<Villa> villas)
+        {
+            listViewVillas.Items.Clear();
+
+            foreach (var villa in villas)
+            {
+                var item = new ListViewItem(villa.Id.ToString());
+                item.SubItems.Add(villa.Name);
+                item.SubItems.Add(villa.Details);
+                item.SubItems.Add(villa.Rate.ToString());
+                item.SubItems.Add(villa.Sqft.ToString());
+                item.SubItems.Add(villa.Occupancy.ToString());
+                item.SubItems.Add(villa.ImageUrl);
+                item.SubItems.Add(villa.Amenity);
+                listViewVillas.Items.Add(item);
+            }
+        }
+
+        private async void btnAdd_Click(object sender, EventArgs e)
+        {
+            var newVilla = new Villa
+            {
+                Name = txtName.Text,
+                Details = txtDetails.Text,
+                Rate = decimal.Parse(txtRate.Text),
+                Sqft = int.Parse(txtSqft.Text),
+                Occupancy = int.Parse(txtOccupancy.Text),
+                ImageUrl = txtImageUrl.Text,
+                Amenity = txtAmenity.Text
+            };
+
+            await CreateVilla(newVilla);
+            await LoadVillas();
+        }
+
+        private async Task CreateVilla(Villa villa)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(villa);
+                HttpResponseMessage response = await httpClient.PostAsync(apiUrl, new StringContent(json, Encoding.UTF8, "application/json"));
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Failed to add villa: " + response.ReasonPhrase, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to add villa: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (listViewVillas.SelectedItems.Count > 0)
+            {
+                int villaId = int.Parse(listViewVillas.SelectedItems[0].Text);
+                var updatedVilla = new Villa
+                {
+                    Id = villaId,
+                    Name = txtName.Text,
+                    Details = txtDetails.Text,
+                    Rate = decimal.Parse(txtRate.Text),
+                    Sqft = int.Parse(txtSqft.Text),
+                    Occupancy = int.Parse(txtOccupancy.Text),
+                    ImageUrl = txtImageUrl.Text,
+                    Amenity = txtAmenity.Text
+                };
+
+                await UpdateVilla(updatedVilla);
+                await LoadVillas();
+            }
+            else
+            {
+                MessageBox.Show("Please select a villa to update.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task UpdateVilla(Villa villa)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(villa);
+                HttpResponseMessage response = await httpClient.PutAsync($"{apiUrl}/{villa.Id}", new StringContent(json, Encoding.UTF8, "application/json"));
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Failed to update villa: " + response.ReasonPhrase, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to update villa: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listViewVillas.SelectedItems.Count > 0)
+            {
+                int villaId = int.Parse(listViewVillas.SelectedItems[0].Text);
+                await DeleteVilla(villaId);
+                await LoadVillas();
+            }
+            else
+            {
+                MessageBox.Show("Please select a villa to delete.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task DeleteVilla(int id)
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.DeleteAsync($"{apiUrl}/{id}");
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Failed to delete villa: " + response.ReasonPhrase, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to delete villa: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public decimal Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
